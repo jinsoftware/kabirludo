19fe7faadd0afb9526f3f484804688b6a7a000bb
jinsoftware <j4269631@gmail.com>
2025-05-22 23:41:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function rollDice() {
   const dice = Math.floor(Math.random() * 6) + 1;
-  document.getElementById("dice-value").innerText = "You rolled: " + dice;
+  document.getElementById("dice-value").innerText = "Dice: " + dice;
 }
